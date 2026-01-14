a1c8fdfc69f1f68a62332cdd909d4423214021ce
hx <im@bgzo.cc>
2026-01-14 21:33:08 +0800

    feat: add sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
-import partytown from '@astrojs/partytown'
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://one.bgzo.cc',
   output: 'static',
   integrations: [
     vue(),
-    partytown({ config: { forward: ['dataLayer.push', 'gtag'] } })
+    partytown({ config: { forward: ['dataLayer.push', 'gtag'] } }),
+    sitemap()
   ],
 });
